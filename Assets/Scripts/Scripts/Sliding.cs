8beec9548489408e4a117ce402a157e9e8b8e9d3
JTor355 <77556051+JTor355@users.noreply.github.com>
2022-06-28 20:19:11 +0800

    tutorial stage
    kindly switch up the values ni player for the balancing ty!

--- Assets/Scripts/Scripts/Sliding.cs
+++ Assets/Scripts/Scripts/Sliding.cs
@@ -2,9 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-/// added if(pm.wallrunning) return;
-
 public class Sliding : MonoBehaviour
 {
     [Header("References")]
