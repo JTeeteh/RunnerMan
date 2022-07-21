75e55d48f4d9adb71c613f6df288af45c2eb031c
Hasumeki <107523780+Hasumeki@users.noreply.github.com>
2022-07-22 01:49:09 +0800

    Stage Update 2.0
    More claustrophobia, more cubes

---
+++ Assets/Scripts/Enemy Ai/Moving.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : Traps
+{
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    protected override void Trigger()
+    {
+
+
+    }
+
+}
