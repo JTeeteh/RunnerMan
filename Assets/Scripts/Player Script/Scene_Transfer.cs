309884b2321164ee4bebab83cbe74e0af1eb5162
PieYOLO <noobulous3301@gmail.com>
2022-07-27 19:34:03 +0800

    I make GLitch go br br br
    KEANU REEVEESS LLALAABBBB YYYYUUUUUUU

--- Assets/Scripts/Player Script/Scene_Transfer.cs
+++ Assets/Scripts/Player Script/Scene_Transfer.cs
@@ -2,25 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Kino;
 public class Scene_Transfer : MonoBehaviour
 {
+    [SerializeField] private DigitalGlitch GlitchEffect;
+    [SerializeField] private float Intensity;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    
     private void OnTriggerEnter(Collider collision)
     {
-        if (collision.gameObject.tag == "Enemy")
+        if (collision is BoxCollider)
         {
             SceneManager.LoadScene("GameOver");
         }
 
     }
+    
+
+
+
+
+
+    private void OnTriggerStay(Collider collider)
+    {
+        if (collider is SphereCollider)
+        {
+            {
+                Vector3 distanceVector = collider.transform.position - transform.position;
+
+                GlitchEffect.intensity = Intensity / distanceVector.magnitude;
+            }
+        }
+    }
+    private void OnTriggerExit(Collider collider)
+    {
+        if (collider is SphereCollider)
+        {
+            GlitchEffect.intensity = 0;
+        }
+    }
+
 }
